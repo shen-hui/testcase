9112b5d1e6819cbbe2855d5576d8b5c21006ef7a
shen-hui <39322041+shen-hui@users.noreply.github.com>
2021-08-31 14:19:49 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 echo starting to run wget...
 /etc/init.d/atd start
-at 14:20 <<< "mkdir /tmp/test" 
+at 14:25 <<< "mkdir /tmp/test" 
 #python /tmp/testcase/test1.py 
 #nohup python -u /tmp/testcase/test1.py >/tmp/log.txt 2>&1 &
